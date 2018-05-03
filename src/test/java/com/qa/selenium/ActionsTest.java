c3fe68a7714548d614bf0b0fa5c85b6aed36367f
Nick <nickgmorgan94@gmail.com>
2018-05-03 09:31:52 +0100

    actions added

--- src/test/java/com/qa/selenium/ActionsTest.java
+++ src/test/java/com/qa/selenium/ActionsTest.java
@@ -16,7 +16,7 @@ import com.qa.selenium.DDTReadWriteFromFile.Browser;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
-public class Actionstt {
+public class ActionsTest {
 
 	WebDriver driver;
 	
@@ -59,21 +59,28 @@ public class Actionstt {
 		
 		driver.get(URL);
 		
-		driver.findElement(By.xpath(
-				"//*[@id=\"menu-item-140\"]/a"))
-				.click();
-		
+		driver.findElement(By.xpath("//*[@id=\"menu-item-140\"]/a")).click();	
 		WebElement draggable = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
-		
 		action.moveToElement(draggable).clickAndHold().moveByOffset(300, 0).perform();
 		
+		driver.findElement(By.xpath("//*[@id=\"menu-item-143\"]/a")).click();
+		WebElement resize = driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+		action.moveToElement(resize).click().clickAndHold().moveByOffset(300, 0).perform();
+		
+		
 		driver.findElement(By.xpath("//*[@id=\"menu-item-141\"]/a")).click();
+		WebElement droppable = driver.findElement(By.xpath("//*[@id=\"draggableview\"]"));
+		action.moveToElement(droppable).click().clickAndHold().moveByOffset(150, 0).perform();
+		Thread.sleep(1000);
 		
-		WebElement droppable = driver.findElement(By.xpath("//*[@id=\"draggableview\"]/p"));
+		driver.findElement(By.xpath("//*[@id=\"menu-item-142\"]/a")).click();
+		WebElement select7 = driver.findElement(By.xpath("//*[@id=\"selectable\"]/li[7]"));
+		action.moveToElement(select7).click().clickAndHold().moveByOffset(0, -150).perform();
 		
 		
-		
-		
+		driver.findElement(By.xpath("//*[@id=\"menu-item-151\"]/a")).click();
+		WebElement sortable1 = driver.findElement(By.xpath("//*[@id=\"sortable\"]/li[1]"));
+		action.moveToElement(sortable1).click().dragAndDropBy(sortable1, 0, 150).perform();
 		
 		
 		Thread.sleep(2000);
